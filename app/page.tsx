c63bce58ab04f8916fb23080622a4269e0413e2e
itrapshorts <cryptodedefi@protonmail.com>
2024-09-24 09:28:54 -0600

    moved trade-streak component to dashbaord

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
-import { getServerSession } from "next-auth/next";
-import { redirect } from "next/navigation";
-import { TradeStreak } from "@/components/trade-streak";
+import { TradeStreak } from "./dashboard/trade-streak"
 
-export default async function Home() {
-  const session = await getServerSession();
-  if (!session) {
-    redirect("/auth/signin");
-  }
-
+export default function HomePage() {
   return (
-    <main className="min-h-screen bg-[#0f0f0f] text-white py-8">
+    <div>
       <TradeStreak />
-    </main>
-  );
+    </div>
+  )
 }
